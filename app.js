828b4491717fa722149d15065bff3bc26f82e56a
nagmabano <bano.nagma.1@gmail.com>
2018-08-11 23:32:17 +0530

    clear input

--- app.js
+++ app.js
@@ -117,6 +117,20 @@ var UIController = (function(){
             
         },
         
+        clearFields: function(){
+          var fields, fieldsArr;
+            
+            fields = document.querySelectorAll(DOMStrings.inputDescription + ',' + DOMStrings.inputValue);
+            
+            fieldsArr = Array.prototype.slice.call(fields);
+            
+            fieldsArr.forEach(function(current, index, array){
+                current.value = "";
+            
+            fieldsArr[0].focus();
+            });
+        },
+        
         getDOMString: function(){
             return DOMStrings;
         }
@@ -162,9 +176,12 @@ var controller = (function(budgetCtrl,UICtrl){
         // 3. Add the item to the UI
         UICtrl.addListItem(newItem, input.type);
         
-        // 4. Calculate the budget
+        // 4. Clear the fields
+        UICtrl.clearFields();
         
-        // 5. Display the budget on the UI
+        // 5. Calculate the budget
+        
+        // 6. Display the budget on the UI
         
         
     };
@@ -175,11 +192,6 @@ var controller = (function(budgetCtrl,UICtrl){
                 setupEventListeners();
             }
         };
-    
-    
-    
-    
-    
     
 })(budgetController,UIController);
     
