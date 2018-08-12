4cc7c0d938204de9915da2c1ec430f2f12b3345c
nagmabano <bano.nagma.1@gmail.com>
2018-08-12 15:22:59 +0530

    setting up event delegation

--- app.js
+++ app.js
@@ -122,7 +122,8 @@ var UIController = (function(){
         budgetLabel: '.budget__value',
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
-        percentageLabel: '.budget__expenses--percentage'
+        percentageLabel: '.budget__expenses--percentage',
+        container: '.container'
         
     }
     
@@ -142,10 +143,10 @@ var UIController = (function(){
             // Create HTML string with placeholder text
             if(type === 'inc'){
                 element = DOMStrings.incomeContainer;                
-              html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';  
+              html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';  
             }else if(type === 'exp'){
                 element = DOMStrings.expenseContainer;
-               html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'; 
+               html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'; 
             }
             
             // Replace the placeholder text with some actual data
@@ -217,6 +218,8 @@ var controller = (function(budgetCtrl,UICtrl){
              }
         
         });
+        
+        document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem)
                 
     };
     
@@ -257,6 +260,21 @@ var controller = (function(budgetCtrl,UICtrl){
         }    
         
     };
+    
+    var ctrlDeleteItem = function(event){
+        var itemID, splitID;
+        
+        itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
+        
+        if(itemID){
+            // inc-1
+            
+            splitID = itemID.split('-');
+            type = splitID[0];
+            ID = splitID[1];
+            
+        }
+    }
     
         return{
             init: function(){
